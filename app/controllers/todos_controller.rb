7b8112649fded9539e24b3aa9a1afa6936b809e5
Keitaro <fe08108@gmail.com>
2018-10-24 16:00:13 +0800

    complete todo feature

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,5 +9,35 @@ class TodosController < ApplicationController
 		@todo = Todo.new
 	end
 
-	
+	def create
+		@todo = current_user.todos.new(todo_params)
+
+		if @todo.save
+			redirect_to todos_path
+		else
+			redirect_to new_todo_path
+		end
+	end
+
+	def edit
+		@todo = Todo.find(params[:id])
+		render 'todos/edit'
+	end
+
+	def update
+		todo = Todo.find(params[:id])
+		todo.update(todo_params)
+		redirect_to todos_path
+	end
+
+	def destroy
+		todo = Todo.find(params[:id])
+		todo.destroy
+		redirect_to todos_path
+	end
+
+	private
+	def todo_params
+		params.require(:todo).permit(:title, :due_date, :priority)
+	end
 end
