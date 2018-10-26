6b68864710f584a346cb97c21c04be28fa9b787a
Keitaro <fe08108@gmail.com>
2018-10-26 14:13:33 +0800

    user can attach gif to individual task

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -3,7 +3,7 @@
 // };
 
 $(document).ready(function(){
-	$('.modal-show').click(function(event){
+	$('[id^=modal-show]').click(function(event){
 		event.preventDefault();
 		let e = $(this);
 		let todoId = (e.parent()[0].id)
@@ -15,7 +15,8 @@ $(document).ready(function(){
 			success: function(data){
 				let title = data.title
 				let due = data.due_date
-				$('#modal-title').html('<i class="circle outline icon"></i>' + title)
+
+				$(`#modal-title${todoId}`).html('<i class="circle outline icon"></i>' + title)
 
 				var d = new Date(due);
 				var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -24,56 +25,59 @@ $(document).ready(function(){
 				var time = d.toLocaleTimeString().toLowerCase().replace(/([\d]+:[\d]+):[\d]+(\s\w+)/g, "$1$2");
 
 
-				$('#modal-due').html(date + " at " + time)
-				$('.ui.modal').modal('show');
+				$(`#modal-due${todoId}`).html(date + " at " + time)
+
+				$(`#modal${todoId}`).modal('show');
 			}
 		})
 	});
-	$('#add-gif').click(function(event){
+	$('[id^=add-gif]').click(function(event){
+		let e = $(this)
+		let gifId = e[0].id
 		event.preventDefault();
-		$('#add-gif').hide();
-		$('#hidden-gif-search').fadeToggle();
-		$('.close.icon').click(function(){
-			$('#add-gif').show();
-			$('#hidden-gif-search').hide();
+		$(`#${gifId}`).hide();
+		$(`#hidden-gif-search-${gifId}`).fadeToggle();
+		$(`#close-icon-in-add-gif${gifId}`).click(function(){
+			$(`#${gifId}`).show();
+			$(`#hidden-gif-search-${gifId}`).hide();
 		});
 	});
-	$('.ui.search.gif')
-	  .search({
-	    apiSettings: {
-	    	onResponse: function(myResponse) {
-	    		let response = {
-	    			results: []
-	    		}
-	    		$.each(myResponse.data, function(index, item){
-	    			response.results.push({
-	    				title: item.title,
-	    				image: item.images.original.url,
-	    			})
-	    		})
-	    		console.log(response)
-	    		return response
-	    	},
-	      url: "http://api.giphy.com/v1/gifs/search?q={query}&api_key=vR83KGIDt6gFnq0SNXtG7UsKM3GWXQox&limit=5"
-	    },
-	    fields: {
-	      results : 'results',
-	      title   : 'title',
-	      image   : "image"
-	    },
-	    minCharacters : 3,
-	    onSelect: function(result) {
-
-	    	$('.description').append(`<img src="${result.image}">`)
-	    }
-	 });
+	$('[id^=hidden-gif-search-add-gif]').keyup(function(){
+		let e = $(this)
+		let gifId = e.parent().children('div')[0].id
+		console.log(gifId)
+		$('.ui.search.gif')
+		  .search({
+		    apiSettings: {
+		    	onResponse: function(myResponse) {
+		    		let response = {
+		    			results: []
+		    		}
+		    		$.each(myResponse.data, function(index, item){
+		    			response.results.push({
+		    				title: item.title,
+		    				image: item.images.original.url,
+		    			})
+		    		})
+		    		return response
+		    	},
+		      url: "http://api.giphy.com/v1/gifs/search?q={query}&api_key=vR83KGIDt6gFnq0SNXtG7UsKM3GWXQox&limit=5"
+		    },
+		    fields: {
+		      results : 'results',
+		      title   : 'title',
+		      image   : "image"
+		    },
+		    minCharacters : 3,
+		    onSelect: function(result) {
+		    	console.log(result)
+		    	$(`#description-${gifId}`).append(`<img src="${result.image}">`)
+		    }
+		 });
+	});
 
 });
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=vR83KGIDt6gFnq0SNXtG7UsKM3GWXQox&limit=5");
-xhr.done(function(giphy) { 
-	console.log(giphy.data[0].images.original.url)
-});
 
 
 
