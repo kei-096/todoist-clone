448790bcfb8e3439f3fc4039fdfb3649678173fa
Keitaro <fe08108@gmail.com>
2018-10-26 12:47:27 +0800

    giphy api integrated

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -29,5 +29,52 @@ $(document).ready(function(){
 			}
 		})
 	});
+	$('#add-gif').click(function(event){
+		event.preventDefault();
+		$('#add-gif').hide();
+		$('#hidden-gif-search').fadeToggle();
+		$('.close.icon').click(function(){
+			$('#add-gif').show();
+			$('#hidden-gif-search').hide();
+		});
+	});
+	$('.ui.search.gif')
+	  .search({
+	    apiSettings: {
+	    	onResponse: function(myResponse) {
+	    		let response = {
+	    			results: []
+	    		}
+	    		$.each(myResponse.data, function(index, item){
+	    			response.results.push({
+	    				title: item.title,
+	    				image: item.images.original.url,
+	    			})
+	    		})
+	    		console.log(response)
+	    		return response
+	    	},
+	      url: "http://api.giphy.com/v1/gifs/search?q={query}&api_key=vR83KGIDt6gFnq0SNXtG7UsKM3GWXQox&limit=5"
+	    },
+	    fields: {
+	      results : 'results',
+	      title   : 'title',
+	      image   : "image"
+	    },
+	    minCharacters : 3,
+	    onSelect: function(result) {
+
+	    	$('.description').append(`<img src="${result.image}">`)
+	    }
+	 });
+
 });
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=vR83KGIDt6gFnq0SNXtG7UsKM3GWXQox&limit=5");
+xhr.done(function(giphy) { 
+	console.log(giphy.data[0].images.original.url)
+});
+
+
+
 
